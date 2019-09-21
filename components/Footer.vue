b8ac9d4753ce012a435f05fa6ddb97e41011b533
Patrick McGrath <pmcgrath@farelogix.com>
2019-09-21 12:30:59 -0400

    editing title

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,13 +1,9 @@
 <template>
-  <footer
-    role="contentinfo"
-    :style="{'background-image': 'url(' + require('@/static/crossword.png') + ')'}"
-  >
+  <footer role="contentinfo">
     <div class="container">
-      <div class="footer-content">
-        &copy; Agape Christian Fellowship 2019
-      </div>
+      <div class="footer-content">&copy; Agape Christian Fellowship 2019</div>
     </div>
+    <!-- :style="{'background-image': 'url(' + require('@/static/crossword.png') + ')'}" -->
   </footer>
 </template>
 <script>
