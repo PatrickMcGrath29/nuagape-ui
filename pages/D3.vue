55b8d6d2dd3c0894ae519890ece43fee10ab51ab
Patrick McGrath <patrickmcgrath29@gmail.com>
2020-01-23 17:17:42 -0500

    Fixing some uploads

---
+++ pages/D3.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="d3-form__wrapper">
+    <div class="container">
+      <iframe
+      src="https://docs.google.com/forms/d/e/1FAIpQLSf-LRpL0h83pX5g9repFw6aVkPJo8gNrjFfhkgE9-CVAdbPug/viewform?embedded=true"
+      width="100%"
+      height="2040"
+      frameborder="0"
+      marginheight="0"
+      marginwidth="0"
+      >Loading…</iframe>
+    </div>
+  </div>
+</template>
